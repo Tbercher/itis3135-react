09ff74ac3f7ace20da69581fea52592be24fedee
Trevor Bercher <trevorbercher@gmail.com>
2026-03-26 09:29:53 -0400

    Feat: setup react base

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import { HashRouter, Routes, Route } from "react-router";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
-      <Routes>
-        <Route element={<App />} path="/"></Route>
-      </Routes>
-    </HashRouter>
+    <App />
   </StrictMode>,
-);
+)
